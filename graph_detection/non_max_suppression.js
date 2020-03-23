f94893d86201c0fe568798e0c85b262da763c1ae
Mo <mohamad@m0.wtf>
2020-03-23 08:03:50 +0000

    Update non_max_suppression.js

--- graph_detection/non_max_suppression.js
+++ graph_detection/non_max_suppression.js
@@ -1,4 +1,9 @@
 function supress_non_max(g) {
+  //g : array returned from matrix
+  // g[0]: gradient image
+  // g[1]: gradient direction array
+  
+  //initialise new image
   let Z = Array.from(Array(g[0].length), () => new Array(g[0][0].length).fill(0));
   let img = g[0];
 
@@ -8,12 +13,14 @@ function supress_non_max(g) {
 
   for (let x = 1; x < w - 1; x++) {
     for (let y = 1; y < h - 1; y++) {
-
+      
+      //no negative angles
       let a = dir[x][y] >= 0 ? dir[x][y] : dir[x][y] + PI;
-      a = round(a / QUARTER_PI);
+      
+      a = round(a / QUARTER_PI); //which quarter 
 
-      q = 255;
-      r = 255;
+      q = 255; //adjacent pixel in positive direction
+      r = 255; //adjacent pixel in negative direction
 
       if (a == 0|| a == 4) {
         q = img[x + 1][y];
@@ -33,6 +40,7 @@ function supress_non_max(g) {
       if (img[x][y] >= q && img[x][y] >= r) {
         Z[x][y] = img[x][y];
       } else {
+        //pixel is suppressed
         Z[x][y] = 0;
       }
     }
